22a9bd8a019ab84f819fd800fc261da337d4dfe3
eya ridene <eya.ridene@insat.u-carthage.tn>
2023-12-09 21:14:29 +0100

    directive ngStyle

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,10 +6,12 @@ import { PageDetailComponent } from './exCv/page-detail/page-detail.component';
 import { LoginComponent } from './login/login.component';
 //import { loginGuard } from './login/login.guard';
 import { HomeComponent } from './components/home/home.component';
+import { StyleComponent } from './directives/style/style.component';
 
 const APP_ROUTES: Routes = [
   //{ path: '', component: HomeComponent },
-  { path: '', component: CvComponent },
+  //{ path: '', component: CvComponent },
+  //{ path: '', component: StyleComponent },
   //{ path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'cv/:id', component: PageDetailComponent },
--- src/app/directives/style/style.component.ts
+++ src/app/directives/style/style.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-style',
   templateUrl: './style.component.html',
-  styleUrls: ['./style.component.css']
+  styleUrls: ['./style.component.css'],
 })
 export class StyleComponent {
+  @Input() color = 'white';
+  @Input() bgColor = 'lightBlue';
+  size = '25px';
 
+  changeSize(sizeInput: any) {
+    this.size = sizeInput + 'px';
+  }
 }

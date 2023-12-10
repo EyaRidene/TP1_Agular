131cffb09be91c572ca9223eb49d85a95f6adc51
eya ridene <eya.ridene@insat.u-carthage.tn>
2023-12-10 01:16:03 +0100

    routing + navbar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,12 +7,14 @@ import { LoginComponent } from './login/login.component';
 //import { loginGuard } from './login/login.guard';
 import { HomeComponent } from './components/home/home.component';
 import { StyleComponent } from './directives/style/style.component';
+import { ClassComponent } from './directives/class/class.component';
 
 const APP_ROUTES: Routes = [
-  //{ path: '', component: HomeComponent },
-  { path: '', component: CvComponent },
-  //{ path: '', component: StyleComponent },
-  //{ path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'cv', component: CvComponent },
+  { path: 'ngStyle', component: StyleComponent },
+  { path: 'class', component: ClassComponent },
+  //{ path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'cv/:id', component: PageDetailComponent },
   { path: 'ex1', component: Ex1Component },

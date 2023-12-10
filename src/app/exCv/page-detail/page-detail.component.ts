d02cdd18aaa3ad30a98a3f3de45c6ede8a83898b
eya ridene <eya.ridene@insat.u-carthage.tn>
2023-12-10 22:27:22 +0100

    delete cv

--- src/app/exCv/page-detail/page-detail.component.ts
+++ src/app/exCv/page-detail/page-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CvService } from '../services/cv.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Cv } from '../model/cv.model';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-page-detail',
@@ -9,17 +11,47 @@ import { Cv } from '../model/cv.model';
   styleUrls: ['./page-detail.component.css'],
 })
 export class PageDetailComponent implements OnInit {
-  cv: Cv | undefined = undefined;
+  cv: Cv | null = null;
+  id: string | null = null;
   constructor(
-    private cvService: CvService,
-    private activatedRoute: ActivatedRoute
+    //private cvService: CvService,
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
-      console.log(params);
-      this.cv = this.cvService.getCvById(params['id']);
-      console.log(this.cv);
+      //this.cv = this.cvService.getCvById(params['id']);
+      this.id = params['id'];
     });
+    this.http
+      .get<Cv>(`https://apilb.tridevs.net/api/personnes/${this.id}`)
+      .subscribe(
+        (cv) => {
+          console.log(cv);
+          this.cv = cv;
+        },
+        (error) => {
+          this.cv = null;
+          this.toastr.error('Erreur lors de la récupération du cv');
+        }
+      );
+  }
+
+  deleteCv(id: number) {
+    this.http.delete(`https://apilb.tridevs.net/api/personnes/${id}`).subscribe(
+      () => {
+        this.toastr.success('Cv supprimé avec succès');
+        this.router.navigate(['/cv']);
+      },
+      (error) => {
+        this.toastr.error('Erreur lors de la suppression du cv');
+        setTimeout(() => {
+          this.router.navigate(['/cv']);
+        }, 2000);
+      }
+    );
   }
 }
